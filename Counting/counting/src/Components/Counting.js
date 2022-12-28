4de02e099c29fa65215c0495a2e5df10834e643b
Yağızcan Şahin <yagizcansahin68@gmail.com>
2022-12-28 10:54:00 +0300

    Finish Porject

--- Counting/counting/src/Components/Counting.js
+++ Counting/counting/src/Components/Counting.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 
 function Counting() {
+  let [number, setNumber] = useState(JSON.parse(localStorage.getItem("value" || 0)))
+
+  useEffect(() => {
+    localStorage.setItem("value", JSON.stringify(number))
+  }, [number])
+
   return (
     <div>
       <h1>Counting</h1>
+      <h2>{number}</h2>
+      <button onClick={() => setNumber(number += 1)}>Increase</button>
+      <button onClick={() => setNumber(number -= 1)}>Decrease</button>
     </div>
   )
 }
