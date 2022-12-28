d120f46b0e6849c042c6d481e3b633bab2917a49
Yağızcan Şahin <yagizcansahin68@gmail.com>
2022-12-28 10:28:10 +0300

    Start working Counting Project

---
+++ Counting/counting/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Counting from './Components/Counting';
+
+function App() {
+  return (
+    <div>
+      <Counting />
+    </div>
+  );
+}
+
+export default App;
---
+++ Counting/counting/src/Components/Counting.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Counting() {
+  return (
+    <div>
+      <h1>Counting</h1>
+    </div>
+  )
+}
+
+export default Counting
